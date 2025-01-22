474b3d4c28128a15f27dbcca15129b2b72907863
WingonWu <7057811+wingonwu@users.noreply.github.com>
2025-01-22 16:19:19 +0800

    Update u-boot.mk

--- configfiles/u-boot.mk
+++ configfiles/u-boot.mk
@@ -1,111 +1,181 @@
-PKG_NAME ?= u-boot
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+include $(TOPDIR)/rules.mk
+include $(INCLUDE_DIR)/kernel.mk
 
-ifndef PKG_SOURCE_PROTO
-PKG_SOURCE = $(PKG_NAME)-$(PKG_VERSION).tar.bz2
-PKG_SOURCE_URL = \
-	https://github.com/xiaomeng9597/files/releases/download/u-boot-2021.07 \
-	https://xiaomeng9597.github.io/files \
-	https://raw.githubusercontent.com/xiaomeng9597/files/main
+PKG_VERSION:=2021.07
+PKG_RELEASE:=$(AUTORELEASE)
+
+PKG_HASH:=skip
+
+PKG_MAINTAINER:=Tobias Maedel <openwrt@tbspace.de>
+
+include $(INCLUDE_DIR)/u-boot.mk
+include $(INCLUDE_DIR)/package.mk
+include ../arm-trusted-firmware-rockchip-vendor/atf-version.mk
+
+define U-Boot/Default
+  BUILD_TARGET:=rockchip
+  UENV:=default
+  HIDDEN:=1
+endef
+
+
+# RK3308 boards
+
+define U-Boot/evb-rk3308
+  BUILD_SUBTARGET:=armv8
+  NAME:=RK3308 EVB
+  BUILD_DEVICES:= \
+    rockchip_rk3308_evb
+  DEPENDS:=+PACKAGE_u-boot-evb-rk3308:trusted-firmware-a-rk3308
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip-vendor
+  ATF:=$(RK3308_ATF)
+  DDR:=$(RK3308_DDR)
+endef
+
+define U-Boot/armsom-rk3308
+  BUILD_SUBTARGET:=armv8
+  NAME:=ArmSoM RK3308 boards
+  BUILD_DEVICES:= \
+    armsom_p2-pro
+  DEPENDS:=+PACKAGE_u-boot-armsom-rk3308:trusted-firmware-a-rk3308
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip-vendor
+  ATF:=$(RK3308_ATF)
+  DDR:=$(RK3308_DDR_UART2)
+endef
+
+
+# RK3328 boards
+
+define U-Boot/nanopi-r2s-rk3328
+  BUILD_SUBTARGET:=armv8
+  NAME:=NanoPi R2S
+  BUILD_DEVICES:= \
+    friendlyarm_nanopi-r2s
+  DEPENDS:=+PACKAGE_u-boot-nanopi-r2s-rk3328:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3328_bl31.elf
+  OF_PLATDATA:=$(1)
+endef
+
+
+# RK3399 boards
+
+define U-Boot/nanopi-r4s-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=NanoPi R4S
+  BUILD_DEVICES:= \
+    friendlyarm_nanopi-r4s
+  DEPENDS:=+PACKAGE_u-boot-nanopi-r4s-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/nanopi-r4se-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=NanoPi R4SE
+  BUILD_DEVICES:= \
+    friendlyarm_nanopi-r4se
+  DEPENDS:=+PACKAGE_u-boot-nanopi-r4se-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/rock-pi-4-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=Rock Pi 4
+  BUILD_DEVICES:= \
+    radxa_rock-pi-4a
+  DEPENDS:=+PACKAGE_u-boot-rock-pi-4-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/rockpro64-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=RockPro64
+  BUILD_DEVICES:= \
+    pine64_rockpro64
+  DEPENDS:=+PACKAGE_u-boot-rockpro64-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/r08-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=RK3399 R08
+  BUILD_DEVICES:= \
+    rk3399_r08
+  DEPENDS:=+PACKAGE_u-boot-r08-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/tpm312-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=RK3399 TPM312
+  BUILD_DEVICES:= \
+    rk3399_tpm312
+  DEPENDS:=+PACKAGE_u-boot-tpm312-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+define U-Boot/firefly-aioc-ai-rk3399
+  BUILD_SUBTARGET:=armv8
+  NAME:=RK3399 AIOC-AI
+  BUILD_DEVICES:= \
+    rk3399-firefly-aioc-ai
+  DEPENDS:=+PACKAGE_u-boot-firefly-aioc-ai-rk3399:arm-trusted-firmware-rockchip
+  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
+  ATF:=rk3399_bl31.elf
+endef
+
+UBOOT_TARGETS := \
+  evb-rk3308 \
+  nanopi-r4s-rk3399 \
+  nanopi-r4se-rk3399 \
+  rock-pi-4-rk3399 \
+  rockpro64-rk3399 \
+  nanopi-r2s-rk3328 \
+  r08-rk3399 \
+  tpm312-rk3399 \
+  firefly-aioc-ai-rk3399
+
+UBOOT_CONFIGURE_VARS += USE_PRIVATE_LIBGCC=yes
+
+UBOOT_MAKE_FLAGS += \
+  BL31=$(STAGING_DIR_IMAGE)/$(ATF)
+
+UBOOT_MAKE_FLAGS += \
+  $(if $(DDR),TPL_BIN=$(STAGING_DIR_IMAGE)/$(DDR))
+
+define Build/Configure
+	$(call Build/Configure/U-Boot)
+
+ifneq ($(OF_PLATDATA),)
+	mkdir -p $(PKG_BUILD_DIR)/tpl/dts
+	mkdir -p $(PKG_BUILD_DIR)/include/generated
+
+	$(CP) $(PKG_BUILD_DIR)/of-platdata/$(OF_PLATDATA)/dt-plat.c $(PKG_BUILD_DIR)/tpl/dts/dt-plat.c
+	$(CP) $(PKG_BUILD_DIR)/of-platdata/$(OF_PLATDATA)/dt-structs-gen.h $(PKG_BUILD_DIR)/include/generated/dt-structs-gen.h
+	$(CP) $(PKG_BUILD_DIR)/of-platdata/$(OF_PLATDATA)/dt-decl.h $(PKG_BUILD_DIR)/include/generated/dt-decl.h
 endif
 
-PKG_BUILD_DIR = $(BUILD_DIR)/$(PKG_NAME)-$(BUILD_VARIANT)/$(PKG_NAME)-$(PKG_VERSION)
+	$(SED) 's#CONFIG_MKIMAGE_DTC_PATH=.*#CONFIG_MKIMAGE_DTC_PATH="$(PKG_BUILD_DIR)/scripts/dtc/dtc"#g' $(PKG_BUILD_DIR)/.config
+	echo 'CONFIG_IDENT_STRING=" OpenWrt"' >> $(PKG_BUILD_DIR)/.config
+endef
 
-PKG_TARGETS := bin
-PKG_FLAGS:=nonshared
-
-PKG_LICENSE:=GPL-2.0 GPL-2.0+
-PKG_LICENSE_FILES:=Licenses/README
-
-PKG_BUILD_PARALLEL:=1
-
-export GCC_HONOUR_COPTS=s
+define Build/InstallDev
+	$(INSTALL_DIR) $(STAGING_DIR_IMAGE)
+	$(CP) $(PKG_BUILD_DIR)/idbloader.img $(STAGING_DIR_IMAGE)/$(BUILD_VARIANT)-idbloader.img
+	$(CP) $(PKG_BUILD_DIR)/u-boot.itb $(STAGING_DIR_IMAGE)/$(BUILD_VARIANT)-u-boot.itb
+endef
 
 define Package/u-boot/install/default
-	$(CP) $(patsubst %,$(PKG_BUILD_DIR)/%,$(UBOOT_IMAGE)) $(1)/
 endef
 
-Package/u-boot/install = $(Package/u-boot/install/default)
-
-define U-Boot/Init
-  BUILD_TARGET:=
-  BUILD_SUBTARGET:=
-  BUILD_DEVICES:=
-  NAME:=
-  DEPENDS:=
-  HIDDEN:=
-  DEFAULT:=
-  VARIANT:=$(1)
-  UBOOT_CONFIG:=$(1)
-  UBOOT_IMAGE:=u-boot.bin
-endef
-
-TARGET_DEP = TARGET_$(BUILD_TARGET)$(if $(BUILD_SUBTARGET),_$(BUILD_SUBTARGET))
-
-UBOOT_MAKE_FLAGS = \
-	HOSTCC="$(HOSTCC)" \
-	HOSTCFLAGS="$(HOST_CFLAGS) $(HOST_CPPFLAGS) -std=gnu11" \
-	HOSTLDFLAGS="$(HOST_LDFLAGS)" \
-	LOCALVERSION="-OpenWrt-$(REVISION)" \
-	STAGING_PREFIX="$(STAGING_DIR_HOST)" \
-	PKG_CONFIG_PATH="$(STAGING_DIR_HOST)/lib/pkgconfig" \
-	PKG_CONFIG_LIBDIR="$(STAGING_DIR_HOST)/lib/pkgconfig" \
-	PKG_CONFIG_EXTRAARGS="--static" \
-	$(if $(findstring c,$(OPENWRT_VERBOSE)),V=1,V='')
-
-define Build/U-Boot/Target
-  $(eval $(call U-Boot/Init,$(1)))
-  $(eval $(call U-Boot/Default,$(1)))
-  $(eval $(call U-Boot/$(1),$(1)))
-
- define Package/u-boot-$(1)
-    SECTION:=boot
-    CATEGORY:=Boot Loaders
-    TITLE:=U-Boot for $(NAME)
-    VARIANT:=$(VARIANT)
-    DEPENDS:=@!IN_SDK $(DEPENDS)
-    HIDDEN:=$(HIDDEN)
-    ifneq ($(BUILD_TARGET),)
-      DEPENDS += @$(TARGET_DEP)
-      ifneq ($(BUILD_DEVICES),)
-        DEFAULT := y if ($(TARGET_DEP)_Default \
-		$(patsubst %,|| $(TARGET_DEP)_DEVICE_%,$(BUILD_DEVICES)) \
-		$(patsubst %,|| $(patsubst TARGET_%,TARGET_DEVICE_%,$(TARGET_DEP))_DEVICE_%,$(BUILD_DEVICES)))
-      endif
-    endif
-    $(if $(DEFAULT),DEFAULT:=$(DEFAULT))
-    URL:=http://www.denx.de/wiki/U-Boot
-  endef
-
-  define Package/u-boot-$(1)/install
-	$$(Package/u-boot/install)
-  endef
-endef
-
-define Build/Configure/U-Boot
-	+$(MAKE) $(PKG_JOBS) -C $(PKG_BUILD_DIR) $(UBOOT_CONFIGURE_VARS) $(UBOOT_CONFIG)_config
-endef
-
-DTC=$(wildcard $(LINUX_DIR)/scripts/dtc/dtc)
-
-define Build/Compile/U-Boot
-	+$(MAKE) $(PKG_JOBS) -C $(PKG_BUILD_DIR) \
-		CROSS_COMPILE=$(TARGET_CROSS) \
-		$(if $(DTC),DTC="$(DTC)") \
-		$(UBOOT_MAKE_FLAGS)
-endef
-
-define BuildPackage/U-Boot/Defaults
-  Build/Configure/Default = $$$$(Build/Configure/U-Boot)
-  Build/Compile/Default = $$$$(Build/Compile/U-Boot)
-endef
-
-define BuildPackage/U-Boot
-  $(eval $(call BuildPackage/U-Boot/Defaults))
-  $(foreach type,$(if $(DUMP),$(UBOOT_TARGETS),$(BUILD_VARIANT)), \
-    $(eval $(call Build/U-Boot/Target,$(type)))
-  )
-  $(eval $(call Build/DefaultTargets))
-  $(foreach type,$(if $(DUMP),$(UBOOT_TARGETS),$(BUILD_VARIANT)), \
-    $(call BuildPackage,u-boot-$(type))
-  )
-endef
+$(eval $(call BuildPackage/U-Boot))
